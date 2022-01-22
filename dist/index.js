3f1c11c95a0d722afbdcfe2db8f50818e3fb4a8a
mathias <mathiasgheno@gmail.com>
2022-01-21 21:58:33 -0300

    chore: adiciona validacao de personagem

--- dist/index.js
+++ dist/index.js
@@ -4772,8 +4772,22 @@ var __webpack_exports__ = {};
 const axios = __nccwpck_require__(6545);
 const core = __nccwpck_require__(2186);
 
+const VALID = [
+  'bender',
+  'fry',
+  'leela',
+  'dr-zoidberg',
+]
+
+function checkValidChar(char) {
+  if(!VALID.includes(char)) {
+    core.setFailed(`Unknown character ${char}`);
+  }
+}
+
 async function main() {
   const chars = core.getInput('character') || 'dr-zoidberg';
+  checkValidChar(chars);
   const response = await axios.get(`https://futuramaapi.herokuapp.com/api/characters/${chars}/1`);
   console.log(response.data[0]);
   core.setOutput('quote', `${response.data[0].quote}`);
--- src/action.js
+++ src/action.js
@@ -1,8 +1,22 @@
 const axios = require('axios');
 const core = require('@actions/core');
 
+const VALID = [
+  'bender',
+  'fry',
+  'leela',
+  'dr-zoidberg',
+]
+
+function checkValidChar(char) {
+  if(!VALID.includes(char)) {
+    core.setFailed(`Unknown character ${char}`);
+  }
+}
+
 async function main() {
   const chars = core.getInput('character') || 'dr-zoidberg';
+  checkValidChar(chars);
   const response = await axios.get(`https://futuramaapi.herokuapp.com/api/characters/${chars}/1`);
   console.log(response.data[0]);
   core.setOutput('quote', `${response.data[0].quote}`);
